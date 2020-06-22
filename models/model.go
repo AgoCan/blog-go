135db6ef332a6961b3b3e59a53a0e2d4e40a98e3
hank <kslhy456@163.com>
2020-06-22 12:16:18 +0800

    整理好目录，开始填充代码

---
+++ models/model.go
@@ -0,0 +1,41 @@
+package model
+
+// https://gorm.io/zh_CN/
+import (
+	"fmt"
+
+	// 导入mysql驱动
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+
+	"blog-go/config"
+)
+
+// DB db handler
+var DB *sqlx.DB
+
+// InitMysql 初始化数据库
+func InitMysql() (err error) {
+	DB, err = sqlx.Open("mysql", config.MysqlConnect)
+	if err != nil {
+		return err
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		return err
+	}
+
+	DB.SetMaxOpenConns(100)
+	DB.SetMaxIdleConns(16)
+	return nil
+}
+
+// Close 关闭数据库
+func Close() {
+	err := DB.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
